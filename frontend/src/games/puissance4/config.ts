66449ae69b7f887304120915e8bc93b51bb813e5
Michele <michele.florio@ynov.com>
2026-03-19 11:21:24 +0100

    feat: add larry and john to hangman

--- frontend/src/games/puissance4/config.ts
+++ frontend/src/games/puissance4/config.ts
@@ -1,4 +1,5 @@
 export const backendBaseUrl =
-  import.meta.env.VITE_BACKEND_URL && import.meta.env.VITE_BACKEND_URL.trim() !== ''
+  import.meta.env.VITE_BACKEND_URL &&
+  import.meta.env.VITE_BACKEND_URL.trim() !== ""
     ? import.meta.env.VITE_BACKEND_URL
-    : 'http://localhost:3000'
+    : "http://localhost:3003";
